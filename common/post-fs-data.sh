d0d000d47c54af7f84ce8d3daa441d3e3e9a3d3d
Jeroen Beckers <info@dauntless.be>
2018-05-30 10:05:44 +0200

    Create directory if not exists yet

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -3,6 +3,7 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
+mkdir -p $MODDIR/system/etc/security/cacerts
 cp -f /data/misc/user/0/cacerts-added/* $MODDIR/system/etc/security/cacerts/
 
 # This script will be executed in post-fs-data mode
