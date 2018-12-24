18eebfcc3a37df434f088c3252e7045318e9b53a
Jeroen Beckers <info@dauntless.be>
2018-12-24 09:04:07 +0100

    Remove old user certificates before syncing

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -4,6 +4,7 @@
 MODDIR=${0%/*}
 
 mkdir -p $MODDIR/system/etc/security/cacerts
+rm $MODDIR/system/etc/security/cacerts/*
 cp -f /data/misc/user/0/cacerts-added/* $MODDIR/system/etc/security/cacerts/
 
 # This script will be executed in post-fs-data mode
