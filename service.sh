24c6ef233f635112f3de780c58161bee74ba36c2
TheDauntless <info@dauntless.be>
2020-05-08 11:15:11 -0400

    Fix startup copy missing

---
+++ service.sh
@@ -0,0 +1,6 @@
+ui_print "Copying user certificates..."
+
+mkdir -p $MODPATH/system/etc/security/cacerts
+rm -f $MODPATH/system/etc/security/cacerts/*
+cp -f /data/misc/user/0/cacerts-added/* $MODPATH/system/etc/security/cacerts/
+
